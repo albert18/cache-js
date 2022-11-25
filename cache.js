d92e0f85da1b6cc4786189d03b5a974aa850c2ed
albert18 <erertech@gmail.com>
2022-11-25 16:29:14 +0300

    update

---
+++ cache.js
@@ -0,0 +1,18 @@
+﻿class Cache_Reuse {
+    constructor() { }
+    StoreJsonData(key, jsonData) {
+        window.localStorage.setItem(keyname + key, JSON.stringify(jsonData));
+    }
+    GetJsonData(key) {
+        var data = window.localStorage.getItem(keyname + "." + key);
+        if (data == null) return null;
+        return JSON.parse(data);
+    }
+    RemoveItem(key) {
+        window.localStorage.removeItem(keyname + "." + key);
+    }
+    Clear() {
+        window.localStorage.clear();
+    }
+}
+const cache = new Cache_Reuse();
